04409fffce19f44ad88e55851d8ac6ace23eec1d
Ali Muhammad Jafari <138518459+AliDiiv@users.noreply.github.com>
2024-09-16 22:50:41 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { TicTacToe } from './components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
